d310c11a2e15a264429e1beb987b00e0aad4e7b3
donkey_hotei <donkey-hotei@cryptolab.net>
2018-05-17 15:11:25 -0700

    add package.json, with babel dependencies, json loader, and webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+/*
+ * Custom configuration file, intended here to use
+ * a babel to transform the ES6 javascript into something
+ * vanilla that AWS Lambda's NodeJS runtime understands,
+ * as well as use Flow's static type checking, and loading
+ * JSON files.
+ */
+module.exports = {
+    loaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                preset: ['es2015'],
+                plugins: ['syntax-flow', 'transform-flow-strip-types']
+            }
+        },
+        {
+            test: /\.json$/,
+            loader: 'json'
+        }
+    ]
+};
