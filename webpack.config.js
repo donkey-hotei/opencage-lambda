121d3941521ed8f094dffca093f7574267d5ae00
donkey_hotei <donkey-hotei@cryptolab.net>
2018-05-18 12:17:22 -0700

    clean up webpack configuration, add two entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,33 @@
 var path = require('path');
 
-/*
- * Custom configuration file, intended here to use
- * a babel to transform the ES6 javascript into something
- * vanilla that AWS Lambda's NodeJS runtime understands,
- * as well as use Flow's static type checking, and loading
- * JSON files.
- */
-module.exports = {
-    output: {
-        path: path.join(__dirname, 'dist'),
-        library: '[name]',
-        libraryTarget: 'commonjs2',
-        filename: '[name].js'
+const config = {
+    entry: {
+        reverse: path.join(__dirname, 'lambdas/reverse.js'),
+        forward: path.join(__dirname, 'lambdas/forward.js')
     },
     target: 'node',
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    preset: ['es2015'],
-                    plugins: ['syntax-flow', 'transform-flow-strip-types']
-                }
-            },
-            {
-                test: /\.json$/,
-                loader: 'json'
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                                presets: ['flow']
+                        }
+                    }
+                ],
             }
         ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        library: '[name]',
+        libraryTarget: 'commonjs2'
     }
 };
+
+module.exports = config;
